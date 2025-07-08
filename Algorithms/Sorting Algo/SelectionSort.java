5fa88436676ec4c9a2f9282539e3370aa63dc617
Sanket <sanketdesai1971@gmail.com>
2025-07-08 10:02:27 +0530

    Added sorting algo.

---
+++ Algorithms/Sorting Algo/SelectionSort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] nums = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        selectionSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end) {
+        int maxIndex = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > arr[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+}
